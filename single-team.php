00d42477de054f4e06f12df2dd3f6b78af7ee8d1
Robin Green <robin@designingreen.co.uk>
2020-05-05 18:03:44 +0100

    update single template

---
+++ single-team.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * The template for displaying all single posts and attachments
+ */
+
+get_header(); ?>
+			
+	<div class="content">
+		<main class="main">
+
+			<section class="area--bg-white">
+				<div class="grid-container">
+		            <div class="grid-x grid-margin-x small-up-1 medium-up-3 large-up-3">
+		
+					    <?php get_template_part('templates/block--team'); ?>
+
+				   	</div>
+			    </div>
+		    </section>
+
+		</main>
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -5,28 +5,23 @@
 
 get_header(); ?>
 			
-<div class="content">
+	<div class="content">
+		<main class="main">
 
-	<div class="inner-content grid-x grid-margin-x grid-padding-x">
-
-		<main class="main small-12 medium-8 large-8 cell" role="main">
+			<section class="area--bg-white">
+				<div class="grid-container">
+		            <div class="grid-x grid-margin-x small-up-1 medium-up-2 large-up-2">
 		
-		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-		    	<?php get_template_part( 'parts/loop', 'single' ); ?>
-		    	
-		    <?php endwhile; else : ?>
-		
-		   		<?php get_template_part( 'parts/content', 'missing' ); ?>
+				    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				    	<?php get_template_part( 'parts/loop', 'single' ); ?>
+				    <?php endwhile; else : ?>
+				   		<?php get_template_part( 'parts/content', 'missing' ); ?>
+				    <?php endif; ?>
 
-		    <?php endif; ?>
+				   	</div>
+			    </div>
+		    </section>
 
-		</main> <!-- end #main -->
-
-		<?php get_sidebar(); ?>
-
-	</div> <!-- end #inner-content -->
-
-</div> <!-- end #content -->
-
+		</main>
+	</div> <!-- end #content -->
 <?php get_footer(); ?>
